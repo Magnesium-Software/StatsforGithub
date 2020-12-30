7602d474fd843c071e633117e86fda702931e4e7
Matthew Gleich <email@mattglei.ch>
2020-12-29 22:26:02 -0500

    🎉 Init project

---
+++ Stats for GitHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stats for GitHub
+//
+//  Created by Matthew Gleich on 12/29/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stats for GitHub/Stats_for_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stats_for_GitHubApp.swift
+//  Stats for GitHub
+//
+//  Created by Matthew Gleich on 12/29/20.
+//
+
+import SwiftUI
+
+@main
+struct Stats_for_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
